7a6dff69d0859803c207eacebaee7042e8212ab3
DavidOliverius <99708980+DavidOliverius@users.noreply.github.com>
2022-11-14 14:28:56 +1100

    Formatting

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,7 +2,11 @@ import React from 'react';
 import TMDB from '../components/TMDB';
 
 const Home = () => {
-  return <div><TMDB /></div>;
+  return (
+    <div>
+      <TMDB />
+    </div>
+  );
 };
 
 export default Home;
