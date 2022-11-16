c24b658bda5d385654b39fda09b199573f74f081
@Juliaoha <juliaoha@hotmail.com>
2022-11-16 17:30:30 +1100

    Fix styling for banner

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -2,44 +2,60 @@ import React from "react";
 import Paper from "@mui/material/Paper";
 import Grid from "@mui/material/Grid";
 import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
 
 const Banner = ({ movie }) => {
   return (
-    <Grid container height="100%">
-      <Grid item xs={12} height="70%">
-        <Paper
+    <Container sx={{ height: "100%" }} maxWidth={false} disableGutters>
+      <Paper
+        sx={{
+          position: "relative",
+          backgroundColor: "grey.800",
+          color: "#fff",
+          mb: 4,
+          backgroundSize: "cover",
+          backgroundRepeat: "no-repeat",
+          backgroundImage: `url(https://image.tmdb.org/t/p/w1280${movie?.backdrop_path})`,
+          height: "70%",
+        }}
+      >
+        {
+          <img
+            style={{ display: "none" }}
+            src={`https://image.tmdb.org/t/p/w1280${movie?.backdrop_path}`}
+            alt={movie?.title}
+          />
+        }
+        <Box
           sx={{
-            position: "relative",
-            backgroundColor: "grey.800",
-            color: "#fff",
-            mb: 4,
-            backgroundSize: "cover",
-            backgroundRepeat: "no-repeat",
-            backgroundImage: `url(https://image.tmdb.org/t/p/w1280${movie?.backdrop_path})`,
+            position: "absolute",
+            top: 0,
+            bottom: 0,
+            right: 0,
+            left: 0,
+            backgroundColor: "rgba(0,0,0,.3)",
             height: "100%",
           }}
-        >
-          {
-            <img
-              style={{ display: "none" }}
-              src={`https://image.tmdb.org/t/p/w1280${movie?.backdrop_path}`}
-              alt={movie?.title}
-            />
-          }
-          <Box
-            sx={{
-              position: "absolute",
-              top: 0,
-              bottom: 0,
-              right: 0,
-              left: 0,
-              backgroundColor: "rgba(0,0,0,.3)",
-              height: "100%",
-            }}
-          />
-        </Paper>
-      </Grid>
-    </Grid>
+        />
+        <Grid container>
+          <Grid item md={6}>
+            <Box
+              sx={{
+                position: "absolute",
+                bottom: 0,
+                p: { xs: 3, md: 6 },
+                pr: { md: 0 },
+              }}
+            >
+              <Typography component="h1" variant="h4" color="inherit">
+                {movie?.title}
+              </Typography>
+            </Box>
+          </Grid>
+        </Grid>
+      </Paper>
+    </Container>
   );
 };
 
