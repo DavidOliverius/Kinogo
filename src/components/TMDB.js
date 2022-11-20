ff5a56279bc4ccd869fd3e926f02d11bb025143a
DavidOliverius <99708980+DavidOliverius@users.noreply.github.com>
2022-11-21 10:52:16 +1100

    Removed old tailwind styles
    Removed old tailwind styles

--- src/components/TMDB.js
+++ src/components/TMDB.js
@@ -35,30 +35,20 @@ const TMDB = () => {
 
   return (
     <div>
-      <div className="flex justify-between flex-row bg-gray-100 dark:bg-gray-900">
-        <h1 className="font-bold text-7xl items-center justify-center mb-4 text-gray-700 dark:text-gray-200">
-          Movies
-        </h1>
-        <form className="flex gap-3 items-center" onSubmit={searchMovies}>
+      <div>
+        <h1>Movies</h1>
+        <form onSubmit={searchMovies}>
           <input
             placeholder="Search..."
-            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
             type="text"
             onChange={(e) => setSearchKey(e.target.value)}
           ></input>
-          <button
-            className="text-white mr-5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
-            type="submit"
-          >
-            Submit
-          </button>
+          <button type="submit">Submit</button>
         </form>
       </div>
 
-      <div className="">
-        <div className="grid gap-4 bg-gray-100 dark:bg-gray-900 grid-cols-4 mx-4">
-          {renderMovies()}
-        </div>
+      <div>
+        <div>{renderMovies()}</div>
       </div>
     </div>
   );
