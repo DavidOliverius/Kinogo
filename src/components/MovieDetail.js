d84b9522951f169b260e3624de7663cdf4f81877
@Juliaoha <juliaoha@hotmail.com>
2022-11-20 22:25:19 +1100

    Add spinner

--- src/components/MovieDetail.js
+++ src/components/MovieDetail.js
@@ -1,7 +1,7 @@
-import React from 'react';
-import { useParams } from 'react-router-dom';
-import axios from 'axios';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { useParams } from "react-router-dom";
+import axios from "axios";
+import { Link } from "react-router-dom";
 
 const MovieDetail = () => {
   const { id } = useParams();
