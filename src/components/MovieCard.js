6235420ba53fc73546138e6455ff4ef70a4e7421
DavidOliverius <99708980+DavidOliverius@users.noreply.github.com>
2022-11-23 13:50:46 +1100

    Update MovieCard.js

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -5,12 +5,22 @@ const MovieCard = ({ movie }) => {
   const IMAGE_PATH = "https://image.tmdb.org/t/p/w500";
   console.log(movie);
   return (
-    <Link to={`id/${movie.id}`}>
-      {movie.poster_path ? (
-        <img src={`${IMAGE_PATH}${movie.poster_path}`} alt={movie.title} />
-      ) : null}
+    <Link
+      to={`../movie/${movie.id}`}
+      relative="true"
+    >
       <div>
-        <div>{movie.title}</div>
+        {movie.poster_path ? (
+          <img
+            src={`${IMAGE_PATH}${movie.poster_path}`}
+            alt={movie.title}
+          />
+        ) : null}
+        <div>
+          <div>
+            {movie.title} ({movie.release_date.slice(0, 4)})
+          </div>
+        </div>
       </div>
     </Link>
   );
